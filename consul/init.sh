ff3375fc00a9f222509435983d29112912045a21
mrmijan <mijanovic.boki.bojan@gmail.com>
2024-06-24 15:15:26 +0200

    minor script fix

--- consul/init.sh
+++ consul/init.sh
@@ -1,16 +1,1 @@
-#!/bin/sh
-
-# Start Consul agent in the background
-consul agent -dev -client 0.0.0.0 &
-
-# Wait for Consul to be ready
-until consul info; do
-  sleep 1
-done
-
-# Import key-value data
-consul kv import @/consul/init_kv.json
-
-# Keep the foreground process running
-tail -f /dev/null
-
+consul agent -dev -client 0.0.0.0 & until consul info; do sleep 1; done && consul kv import @/consul/init_kv.json && tail -f /dev/null
