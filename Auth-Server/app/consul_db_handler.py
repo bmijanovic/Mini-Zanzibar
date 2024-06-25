24cc5584af280ced84bbeee61ec36a239e147827
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2024-06-25 18:19:54 +0200

    More namespaces implemented

--- Auth-Server/app/consul_db_handler.py
+++ Auth-Server/app/consul_db_handler.py
@@ -12,32 +12,37 @@ class ConsulDBHandler:
         self.loop.create_task(self.initialize())
 
     async def initialize(self):
-        current_version = await self.get("current_version")
-        if current_version is not None:
-            self.config = await self.get(current_version)
-        else:
-            self.config = {}
+        namespaces = await self.get("namespaces")
+        for namespace in namespaces:
+            current_version = await self.get("current_version_" + namespace)
+            self.config[namespace] = await self.get(current_version + "_" + namespace)
 
-    async def create_new_config(self, new_config):
-        latest_version = await self.get("latest_version")
-        if latest_version is None:
-            latest_version = "v0"
-        new_version = 'v' + str(int(latest_version.lstrip("v")) + 1)
-        await self.put(new_version, new_config)
-        await self.put("latest_version", new_version)
+    async def create_new_config(self, namespace, new_config):
+        namespaces = await self.get("namespaces")
+        namespaces.append(namespace)
+        await self.put("namespaces", namespaces)
+
+        new_version = "v1"
+        latest_version = await self.get("latest_version_" + namespace)
+        if latest_version is not None:
+            new_version = 'v' + str(int(latest_version.lstrip("v")) + 1)
+        await self.put(new_version + "_" + namespace, new_config)
+        await self.put("latest_version_" + namespace, new_version)
 
         # set new version for current version
-        await self.put("current_version", new_version)
+        await self.put("current_version_" + namespace, new_version)
 
-    async def change_config_to_version(self, version):
-        found_config = await self.get(version)
+    async def change_config_to_version(self, namespace, version):
+        found_config = await self.get(version + "_" + namespace)
         if found_config is None:
             return False
-        self.config = found_config
+        self.config[namespace] = found_config
         return True
 
-    async def get_config(self):
-        return self.config
+    async def get_config(self, namespace):
+        if namespace not in self.config.keys():
+            return None
+        return self.config[namespace]
 
     async def put(self, key, value):
         value_json = json.dumps(value)
